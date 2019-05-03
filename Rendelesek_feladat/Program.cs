faf69091e662a1d1601bfcaed6e7d9f88de723b9
Kovács Ferenc <kferi702@gmail.com>
2019-05-03 11:08:16 +0200

    Add project files.

---
+++ Rendelesek_feladat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rendelesek_feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
